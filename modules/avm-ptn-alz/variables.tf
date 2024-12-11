4b2738bccd391d4e167b60aa82b402f046fbf111
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-11 18:32:54 +0000

    placement

--- modules/avm-ptn-alz/variables.tf
+++ modules/avm-ptn-alz/variables.tf
@@ -1,24 +1,35 @@
 variable "subscription_id" {
-  type    = string
+  type = string
   default = "6ab5ad42-6a7d-4f21-a926-9ce61b67df96"
 }
 
 variable "tenant_id" {
-  type    = string
-  default = "832b0908-3585-4294-a01c-7763fc195006"
+    type = string
+    default = "832b0908-3585-4294-a01c-7763fc195006"  
 }
 
 variable "location" {
   type = string
-  description = "Azure region where resources will be deployed"
+  default = "uksouth"
 }
 
 variable "architecture_name" {
   type = string
-  description = "Name of the architecture"
+  default = "custom"
 }
 
+
+
 variable "parent_resource_id" {
-  type        = string
-  description = "ID of the parent resource (e.g., resource group or management group)"
+  type = string
+  default = ""
+}
+
+
+variable "subscription_placement" {
+  description = "A map of subscriptions to place into management groups"
+  type = map(object({
+    subscription_id       = string
+    management_group_name = string
+  }))
 }
