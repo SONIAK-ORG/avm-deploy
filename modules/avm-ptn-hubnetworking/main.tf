88098119b7ac86156796f439dfbb6f99b391f87e
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-11 23:30:16 +0000

    Update main.tf

--- modules/avm-ptn-hubnetworking/main.tf
+++ modules/avm-ptn-hubnetworking/main.tf
@@ -1,8 +1,7 @@
 # HubNetworking module
 module "hubnetworking" {
-  source = "Azure/terraform-azurerm-avm-ptn-hubnetworking"
-
-  
+  source = "Azure/terraform-azurerm-avm-ptn-hubnetworking/azurerm"
+  version = 0.4.0  
   location              = var.location
   resource_group_name   = var.resource_group_name
   hub_name              = var.hub_name
