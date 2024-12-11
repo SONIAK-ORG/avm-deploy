e04f5f6e40eeb3559bd1924741305a1879703053
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-11 22:00:23 +0000

    vnet-relocate-3

---
+++ modules/avm-ptn-networking/main.tf
@@ -0,0 +1,22 @@
+# VirtualWAN module
+module "virtualwan" {
+  count  = var.enable_virtualwan ? 1 : 0
+  source = "Azure/terraform-azurerm-avm-ptn-virtualwan"
+  
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  virtualwan_name       = var.virtualwan_name
+  # Add other required variables for VirtualWAN module
+}
+
+# HubNetworking module
+module "hubnetworking" {
+  count  = var.enable_hubnetworking ? 1 : 0
+  source = "Azure/terraform-azurerm-avm-ptn-hubnetworking"
+  
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  hub_name              = var.hub_name
+  # Add other required variables for HubNetworking module
+}
+
---
+++ modules/avm-ptn-networking/variables.tf
@@ -0,0 +1,33 @@
+variable "enable_virtualwan" {
+  description = "Enable Virtual WAN deployment"
+  type        = bool
+  default     = false
+}
+
+variable "enable_hubnetworking" {
+  description = "Enable Hub Networking deployment"
+  type        = bool
+  default     = false
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+# VirtualWAN specific variables
+variable "virtualwan_name" {
+  description = "Name of the Virtual WAN"
+  type        = string
+}
+
+# HubNetworking specific variables
+variable "hub_name" {
+  description = "Name of the Hub Networking Virtual Hub"
+  type        = string
+}
