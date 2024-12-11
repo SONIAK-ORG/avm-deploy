74ea9b24070d7e1c76bbe7898c39cf98445ada65
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-11 11:28:03 +0000

    Update main.tf

--- deployments/avm-ptn-alz/main.tf
+++ deployments/avm-ptn-alz/main.tf
@@ -1,10 +1,10 @@
-data "azapi_client_config" "current" {}
+#data "azapi_client_config" "current" {}
 
 module "avm_ptn_alz" {
   source  = "../../modules/avm-ptn-alz"  # Adjusted for local folder structure
 
   architecture_name  = var.rchitecture_name 
-  parent_resource_id = data.azapi_client_config.current.tenant_id
+  parent_resource_id = "/providers/Microsoft.Management/managementGroups/832b0908-3585-4294-a01c-7763fc195006"     #data.azapi_client_config.current.tenant_id
   location           = var.location 
 
   providers = {
