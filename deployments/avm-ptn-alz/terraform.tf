1d64147ccae10db731dd0f8b90e3a231f1eda7c9
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-11 02:08:42 +0000

    Update terraform.tf

--- deployments/avm-ptn-alz/terraform.tf
+++ deployments/avm-ptn-alz/terraform.tf
@@ -35,10 +35,12 @@ provider "alz" {
     }
   ]
 
-  tenant_id   = var.tenant_id
+  tenant_id   =  "09583a4a-6ecd-403d-a1de-7847ab22da08"
 }
 
 provider "azapi" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+  client_id = "09583a4a-6ecd-403d-a1de-7847ab22da08"
+  use_oidc = true
 }
