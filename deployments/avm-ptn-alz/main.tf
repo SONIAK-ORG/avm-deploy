c42139aeb410e87198dc8c9228adb412d6eb989a
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-11 02:44:42 +0000

    Update main.tf

--- deployments/avm-ptn-alz/main.tf
+++ deployments/avm-ptn-alz/main.tf
@@ -7,6 +7,9 @@ module "avm_ptn_alz" {
   parent_resource_id = data.azapi_client_config.current.tenant_id
   location           = var.location
 
+  providers = {
+    azapi = azure.azapi
+  }
 
   # Add other required module-specific variables as needed
 }
