93c61ac40fe954badb78540966f03a71b9ab153b
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-12 11:08:00 +0000

    Update main.tf

--- modules/avm-ptn-virtualwan/main.tf
+++ modules/avm-ptn-virtualwan/main.tf
@@ -5,6 +5,6 @@ module "avm-ptn-virtualwan" {
   location              = var.location
   resource_group_name   = var.resource_group_name
   virtual_wan_name      = var.virtual_wan_name
-  allow_branch_to_branch_traffic = true
+  allow_branch_to_branch_traffic = var.allow_branch_to_branch_traffic
   # Add other required variables for VirtualWAN module
 }
