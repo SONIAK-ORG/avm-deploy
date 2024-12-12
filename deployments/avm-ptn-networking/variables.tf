b08be5328ed6e6e8a3e93b7fa3ed138b83bccbd4
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-12 12:48:20 +0000

    Update variables.tf

--- deployments/avm-ptn-networking/variables.tf
+++ deployments/avm-ptn-networking/variables.tf
@@ -43,7 +43,7 @@ variable "subscription_id_connectivity" {
 variable "tenant_id" {
   type        = string
   description = "Subscription ID to use for \"connectivity\" resources."
-  default = "09583a4a-6ecd-403d-a1de-7847ab22da08"
+  default = "832b0908-3585-4294-a01c-7763fc195006"
 }
 
  variable "hub_virtual_networks" {
