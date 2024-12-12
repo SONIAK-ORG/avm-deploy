cfa6ffe571fc369c4cc8d750b7d7ff5068b637b9
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-12 16:55:19 +0000

    Update main.tf

--- deployments/avm-ptn-networking/main.tf
+++ deployments/avm-ptn-networking/main.tf
@@ -4,7 +4,7 @@ module "virtualwan" {
   source = "../../modules/avm-ptn-virtualwan"
   
   location                    = var.location
-  resource_group_name         = azurerm_resource_group.rg.name
+  resource_group_name         = var.resource_group_name
   virtual_wan_name            = var.virtual_wan_name
   allow_branch_to_branch_traffic = var.allow_branch_to_branch_traffic
   # Add other required variables for VirtualWAN module
@@ -19,7 +19,7 @@ module "hubnetworking" {
   count  = var.enable_hubnetworking ? 1 : 0
   source = "../../modules/avm-ptn-hubnetworking"
 
-  hub_virtual_networks = local.updated_hub_virtual_networks
+  hub_virtual_networks = var.hub_virtual_networks
 
   providers = {
     azurerm = azurerm
