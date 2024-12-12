92668ff7bf3668d7b2c320625b63f8a2c3e75413
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-12 15:28:09 +0000

    Update variables.tf

--- deployments/avm-ptn-alz/variables.tf
+++ deployments/avm-ptn-alz/variables.tf
@@ -32,8 +32,3 @@ variable "subscription_placement" {
 }
 
 
-variable "tenant_id" {
-  type        = string
-  description = "Subscription ID to use for \"connectivity\" resources."
-  default = "09583a4a-6ecd-403d-a1de-7847ab22da08"
-}
