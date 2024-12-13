75a3ed69ef18b3d4e9a8bf96dcfc0a918ffd78d7
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-13 15:52:32 +0000

    Update main.tf

--- deployments/Lz-Vending/main.tf
+++ deployments/Lz-Vending/main.tf
@@ -1,15 +1,11 @@
 module "landing_zones" {
   source  = "../../modules/Lz-Vending"
 
-  for_each = var.landing_zones
 
-  location                  = each.value.location
+  for_each                 = var.landing_zones
+  location                 = each.value.location
   subscription_display_name = each.value.subscription_display_name
-  #subscription_billing_scope = each.value.subscription_billing_scope
-  #subscription_alias_enabled = each.value.subscription_alias_enabled
-  subscription_id =            each.value.subscription_id
-
-  # Pass virtual networks for each landing zone
-  virtual_networks = each.value.virtual_networks
+  subscription_id          = each.value.subscription_id
+  virtual_networks         = each.value.virtual_networks
 }
 
