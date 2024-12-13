29575b4ea7e796d58935bf9888cf7e3fcab26923
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-13 15:51:45 +0000

    Update main.tf

--- modules/Lz-Vending/main.tf
+++ modules/Lz-Vending/main.tf
@@ -2,15 +2,10 @@ module "landing_zones" {
   source  = "Azure/lz-vending/azurerm"
   version = "4.1.5"
 
-  for_each = var.landing_zones
-
-  location                  = each.value.location
+  for_each                 = var.landing_zones
+  location                 = each.value.location
   subscription_display_name = each.value.subscription_display_name
-  #subscription_billing_scope = each.value.subscription_billing_scope
-  #subscription_alias_enabled = each.value.subscription_alias_enabled
-  subscription_id =            each.value.subscription_id
-
-  # Pass virtual networks for each landing zone
-  virtual_networks = each.value.virtual_networks
+  subscription_id          = each.value.subscription_id
+  virtual_networks         = each.value.virtual_networks
 }
 
