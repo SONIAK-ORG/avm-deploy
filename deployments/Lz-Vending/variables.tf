4cd86763f9e1dc6d27a3773dd4b50fc313c96a22
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-13 19:33:00 +0000

    Update variables.tf

--- deployments/Lz-Vending/variables.tf
+++ deployments/Lz-Vending/variables.tf
@@ -20,3 +20,10 @@ variable "tenant_id" {
   type        = string
   default = "832b0908-3585-4294-a01c-7763fc195006"
 }
+
+
+variable "subscription_id" {
+  type = string
+  default = "6ab5ad42-6a7d-4f21-a926-9ce61b67df96"
+
+}
