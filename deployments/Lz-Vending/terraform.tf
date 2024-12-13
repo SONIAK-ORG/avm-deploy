2562d52ed43003fe5e02e7baefddd2747814634f
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-13 23:37:31 +0000

    Update terraform.tf

--- deployments/Lz-Vending/terraform.tf
+++ deployments/Lz-Vending/terraform.tf
@@ -4,18 +4,31 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.74"
     }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 1.7"
+    }
   }
-
 }
+
 
 
 provider "azurerm" {
   features {
   }
 
-  tenant_id       = var.tenant_id
+  tenant_id  = var.tenant_id
   use_oidc = true
   client_id = "09583a4a-6ecd-403d-a1de-7847ab22da08"
   subscription_id = var.subscription_id
 }
+
+
+provider "azapi" {
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = "09583a4a-6ecd-403d-a1de-7847ab22da08" 
+  use_oidc        = true
+}
+
 
