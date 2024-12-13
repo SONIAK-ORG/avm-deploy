c93b49835f8ad604da6256b06126f448c733d3d2
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-13 18:33:16 +0000

    Update main.tf

--- deployments/Lz-Vending/main.tf
+++ deployments/Lz-Vending/main.tf
@@ -1,11 +1,11 @@
 module "landing_zones" {
   source  = "../../modules/Lz-Vending"
 
-
-  for_each                 = var.landing_zones
-  location                 = each.value.location
-  subscription_display_name = each.value.subscription_display_name
-  subscription_id          = each.value.subscription_id
-  virtual_networks         = each.value.virtual_networks
+   landing_zones =   = var.landing_zones
+#  for_each                 = var.landing_zones
+#  location                 = each.value.location
+#  subscription_display_name = each.value.subscription_display_name
+#  subscription_id          = each.value.subscription_id
+#  virtual_networks         = each.value.virtual_networks
 }
 
