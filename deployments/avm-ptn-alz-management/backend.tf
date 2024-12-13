1f716cd8da651973ecf396fd9acfcebd7bd9ede3
Conor McCann <c.mccann@kainos.com>
2024-12-13 08:34:55 +0000

    Conors changes to management module

--- deployments/avm-ptn-alz-management/backend.tf
+++ deployments/avm-ptn-alz-management/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "terrastatekainos"
     container_name       = "tfstate"
-    key                  = "avm-ptn-alz/terraform.tfstate"
+    key                  = "avm-ptn-alz-management/terraform.tfstate"
     use_oidc             = true  
   }
 }
