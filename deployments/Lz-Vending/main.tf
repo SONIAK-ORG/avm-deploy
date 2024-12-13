41a9cca7303d7c2b06cdbe9626a68e5cfaf77e73
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-13 15:27:45 +0000

    lz-vending-3

---
+++ deployments/Lz-Vending/main.tf
@@ -0,0 +1,15 @@
+module "landing_zones" {
+  source  = "Azure/lz-vending/azurerm"
+  version = "4.1.5"
+
+  for_each = var.landing_zones
+
+  location                  = each.value.location
+  subscription_display_name = each.value.subscription_display_name
+  subscription_billing_scope = each.value.subscription_billing_scope
+  subscription_alias_enabled = each.value.subscription_alias_enabled
+
+  # Pass virtual networks for each landing zone
+  virtual_networks = each.value.virtual_networks
+}
+
