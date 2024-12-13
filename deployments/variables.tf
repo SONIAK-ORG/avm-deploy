1995484d1296620c5dd8479d6b85b07a6300a8b5
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2024-12-13 19:00:22 +0000

    Update variables.tf

--- deployments/variables.tf
+++ deployments/variables.tf
@@ -4,3 +4,9 @@ variable "tenant_id" {
   description = "Subscription ID to use for \"connectivity\" resources."
   default = "09583a4a-6ecd-403d-a1de-7847ab22da08"
 }
+
+
+variable "tenant_id" {
+    type = string
+    default = "832b0908-3585-4294-a01c-7763fc195006"  
+}
